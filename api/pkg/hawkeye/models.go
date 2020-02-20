2e85c7b69f017f2133b7d4ab756f8ec7bed31516
swadhinroutray <swadhin.routray@gmail.com>
2020-02-20 18:10:50 +0530

    Added Model of Elixir

--- api/pkg/hawkeye/models.go
+++ api/pkg/hawkeye/models.go
@@ -6,6 +6,7 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
+//User
 type User struct { //TODO: Fix number of items
 	ID        primitive.ObjectID `bson:"_id"        json:"id"`
 	CreatedAt time.Time          `bson:"created_at" json:"createdAt"`
@@ -18,23 +19,25 @@ type User struct { //TODO: Fix number of items
 	Mobile   string `bson:"mobile"   json:"mobile"`
 	College  string `bson:"college"  json:"college"`
 
-	Level        [7]int  `bson:"level"  json:"level"` //Levels across multiple regions
-	Access       int     `bson:"access" json:"access"`
-	Banned       bool    `bson:"banned" json:"banned"`
-	Inventory    []int   `bson:"inventory" json:"inventory"` // Inventory items implemmentation (How many does each user have?)
-	Points       int     `bson:"points" json:"points"`       //Check how to implement points
-	RegionUnlock string  `bson:"regionunlock" json:"regionunlock"`
-	ItemBool   	 [7]bool `bson:"itembool" json:"itembool"` // Check wether an item has already been used on a question
-	ToBuy        []int   `bson:"tobuy"  json:"tobuy"`          //How many left to be bough for ech usr, if you have doubt call sanchit and ask
+	Level        [7]int   `bson:"level"  json:"level"` //Levels across multiple regions
+	Access       int      `bson:"access" json:"access"`
+	Banned       bool     `bson:"banned" json:"banned"`
+	Inventory    []Elixir `bson:"inventory" json:"inventory"` // Inventory items implemmentation (How many does each user have?)
+	Points       int      `bson:"points" json:"points"`       //Check how to implement points
+	RegionUnlock string   `bson:"regionunlock" json:"regionunlock"`
+	ItemBool     [7]bool  `bson:"itembool" json:"itembool"` // Check wether an item has already been used on a question
+	ToBuy        []int    `bson:"tobuy"  json:"tobuy"`      //How many left to be bough for ech usr, if you have doubt call sanchit and ask
+	History      []Elixir `bson:"history" json:"history"`
 }
 
+//Questions
 type Question struct {
 	ID        primitive.ObjectID `bson:"_id"        json:"id"`
 	CreatedAt time.Time          `bson:"created_at" json:"createdAt"`
 	UpdatedAt time.Time          `bson:"updated_at" json:"updatedAt"`
 
 	Level    int    `bson:"level"    json:"level"`
-	Region   int 	`bson:"region" json: "region"`
+	Region   int    `bson:"region" json: "region"`
 	Question string `bson:"question" json:"question"`
 	Answer   string `bson:"answer"   json:"answer,omitempty"`
 	AddInfo  string `bson:"add_info" json:"addInfo,omitempty"`
@@ -42,6 +45,7 @@ type Question struct {
 	Hints []Hint `bson:"hints" json:"hints"`
 }
 
+//Hint
 type Hint struct {
 	ID        primitive.ObjectID `bson:"_id"        json:"id"`
 	CreatedAt time.Time          `bson:"created_at" json:"createdAt"`
@@ -52,6 +56,7 @@ type Hint struct {
 	Active bool   `bson:"active" json:"active"`
 }
 
+//Submission
 type Submission struct {
 	ID        primitive.ObjectID `bson:"_id"          json:"id"`
 	CreatedAt time.Time          `bson:"created_at" json:"createdAt"`
@@ -63,8 +68,31 @@ type Submission struct {
 	Status   string             `bson:"status"   json:"status"`
 }
 
+//Regions
 type Region struct {
 	ID         primitive.ObjectID `bson:"_id"          json:"id"`
 	CreatedAt  time.Time          `bson:"created_at" json:"createdAt"`
 	UnlockedAt time.Time          `bson:"unlocked_at" json:"unlocked_at"` //Unlocking time for each region
 }
+
+//Inventory
+type Elixir struct {
+	ID       primitive.ObjectID `bson:"_id"          json:"id"`
+	BoughtAt time.Time          `bson:"bought_at" json:"BoughtAt"`
+	UsedAt   time.Time          `bson:"used_at" json:"UsedAt"`
+	Elixir   int                `bson:"elixir" json:"elexir"`
+	Active   bool               `bson:"active"  json:"active"`
+	Region   int                `bson:"region" json:"region"`
+	Question int                `bson:"question" json:"question"`
+}
+
+/*What potions are we keeping?
+- Region Multiplier
+- Hangman
+- Extra Hint
+-
+*/
+/*
+	Every five questions, we have a multiplier for the next one
+
+*/
