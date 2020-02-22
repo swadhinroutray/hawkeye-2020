0cc3227d32603fd54f851efaac580beeb8986ca1
sahaysanchit14 <sahaysanchit14@gmail.com>
2020-02-22 17:04:08 +0530

    Working with DB

--- api/pkg/hawkeye/routes.go
+++ api/pkg/hawkeye/routes.go
@@ -4,4 +4,6 @@ func (app *App) mountRoutes() {
 	api := app.router.PathPrefix("/api").Subrouter()
 
 	api.HandleFunc("/hello", app.HelloWork).Methods("GET")
+	api.HandleFunc("/allusers", app.GetUser).Methods("GET")
+	api.HandleFunc("/adduser", app.AddUser).Methods("POST")
 }
--- api/pkg/hawkeye/work.go
+++ api/pkg/hawkeye/work.go
@@ -4,6 +4,11 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 //Help ...
@@ -20,4 +25,53 @@ func (app *App) HelloWork(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		fmt.Println("Error on route /hello")
 	}
+}
+
+//AddUser ...
+func (app *App) AddUser(w http.ResponseWriter, r *http.Request) {
+	newUser := User{
+		ID:        primitive.NewObjectID(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+
+		Name:     "John Doe",
+		Username: "jdoe",
+	}
+
+	res, err := app.db.Collection("users").InsertOne(r.Context(), newUser)
+	if err != nil {
+		fmt.Printf("Error")
+	}
+	fmt.Printf("Added User: %s", res)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(newUser)
+}
+
+//GetUser ...
+func (app *App) GetUser(w http.ResponseWriter, r *http.Request) {
+
+	findOpts := options.Find()
+
+	cur, err := app.db.Collection("User").Find(r.Context(), bson.D{}, findOpts)
+	if err != nil {
+		fmt.Printf("Database Error:%s", err)
+	}
+
+	var allUsers []User
+
+	err = cur.All(r.Context(), &allUsers)
+	fmt.Println(allUsers)
+	if err != nil {
+		fmt.Printf("Database error:%s", err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	if err := cur.All(r.Context(), &allUsers); err != nil {
+		fmt.Println("Error on route /allusers")
+		return
+	}
+
+	json.NewEncoder(w).Encode(allUsers)
+
 }
