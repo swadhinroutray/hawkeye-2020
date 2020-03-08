a53b70aea89a5686081e158a6c47ad20b08da14d
Dennis Johnson <densarjohn@gmail.com>
2020-03-08 22:42:09 +0530

    Added static assets

---
+++ client/src/assets/landing-assets/index.js
@@ -0,0 +1,20 @@
+import hologramImg from './Hologram.svg';
+import regionImg from './Building.svg';
+import leftBar from './LeftBar.svg';
+import rightBar from './RightBar.svg';
+import slider from './Slider.svg';
+import sliderButton from './SliderButton.svg';
+import waves from './Waves.svg';
+import backgroundMesh from './Background.svg';
+import startButton from './StartButton.svg';
+export {
+	leftBar,
+	rightBar,
+	hologramImg,
+	regionImg,
+	backgroundMesh,
+	waves,
+	slider,
+	sliderButton,
+	startButton,
+};
