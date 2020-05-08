b66e75f5f0fab1105c74154ddbd8514648d30457
chakradhar123 <chakradharvasurama@gmail.com>
2020-05-08 17:15:40 +0530

    style bc

--- client/src/components/Rules.jsx
+++ client/src/components/Rules.jsx
@@ -38,9 +38,11 @@ export default class Rules extends React.Component {
 						<h2 style={{ color: 'white' }}> Welcome to HawkEye'20 </h2>
 
 						<div style={{ color: 'white' }} className="rules-list">
+                        <div style={{textAlign:"center",fontWeight:"bold",color:"white",fontSize:"30px"}}>Gameplay</div>
+                        
 							<div style={{ color: 'white' }}>
 								1) This is an online scavenger hunt, it starts at 9/5/20 
-								12:00 PM and ends on 12/5/20 23:59 PM
+								12:00 PM and ends on 12/5/20 11:59 PM
 							</div>
 							<div style={{ color: 'white' }}>
 								2) Due to a mishap with the time portal, you have been randomly dropped into one of the 5 total timelines that have been created.
@@ -64,50 +66,52 @@ export default class Rules extends React.Component {
 								You will not find anything hidden in the codebase of the game.
 
 							</div>
+							
 							<div style={{ color: 'white' }}>
-								7)You will receive points for every question you solve. 
-								The leaderboard is calculated based on the total points you have, and NOT the number of questions you have solved.
-
-							</div>
-							<div style={{ color: 'white' }}>
-								8)As you progress through these timelines, the points you get for answering a question also increases. 
-								After every third question that you solve, you get 1.5 times more points than you did for the previous ones.
-
-							</div>
-							<div style={{ color: 'white' }}>
-								9) To make it interesting, Hawk presents you with 4 different types of Elixirs, that you can buy using your points.
-								 However, due to a limited supply, there are only a few Elixirs of each kind you can buy. You can only use one Elixir on a particular question.
-
-							</div>
-							<div style={{ color: 'white' }}>
-								10) However, after you reach a certain point, you will be allowed to reset your supply of Elixirs at the cost of your points. 
-								This will however, affect your position on the leaderboard, so use your points wisely.
-							</div>
-							<div style={{ color: 'white' }}>
-								11)Hawk will also let you know if you’re on the right track, by indicating that it thinks you’re close, 
+								7)Hawk will also let you know if you’re on the right track, by indicating that it thinks you’re close, 
 								if your answer contains keywords close to the actual answer, or the correct answer partially. 
 							</div>
 							<div style={{ color: 'white' }}>
-								12) Cheaters will be found by our monitoring system and will be ineligible for any prizes. 
+								8) Cheaters will be found by our monitoring system and will be ineligible for any prizes. 
 								Any suspicious behaviour will be reported to us by the game.
 							</div>
-							<div style={{ color: 'white' }}>
-								13) Winners will be decided on the basis of who has the most points at the end of the game and who got there first. 
-								Therefore, it’s imperative that you buy and use your Elixirs wisely.
-							</div>
-						
-					</div>
+                            <div style={{textAlign:"center",fontWeight:"bold",color:"white",fontSize:"30px"}}>Ranking & Points</div>
+                            <div style={{ color: 'white' }}>
+								1) You will receive points for every question you solve. The leaderboard is calculated based on the total points you have, and NOT the number of questions you have solved.
 
-					<h3>For any issues, contact one of the following</h3>
+							</div>
+							<div style={{ color: 'white' }}>
+								2) As you progress through these timelines, the points you get for answering a question also increases. After every third question that you solve, you get 1.5 times more points than you did for the previous ones.
+							</div>
+                            <div style={{ color: 'white' }}>3) To make it interesting, Hawk presents you with 4 different types of Elixirs, that you can buy using your points. However, due to a limited supply, there are only a few Elixirs of each kind you can buy. You can only use one Elixir on a particular question.</div>
+                            <div style={{ color: 'white' }}>4) However, after you reach a certain point, you will be allowed to reset your supply of Elixirs at the cost of your points. But this will affect your position on the leaderboard, so use your points wisely. 
+</div>
+                            <div style={{ color: 'white' }}>5) Winners will be decided on the basis of who has the most points at the end of the game and who got there first. Therefore, it’s imperative that you buy and use your Elixirs wisely.</div>
+                            <div style={{textAlign:"center",fontWeight:"bold",color:"white",fontSize:"30px"}}>Elixirs</div>
+                            <div style={{color:"#F2AD00",fontWeight:"bold",fontSize:"20px"}}>Extra Hint - 30 points</div>
+                            <div style={{ color: 'white' }}>-  If you’re stuck on a question, you can get an extra hint that is completely different from the default hints of the question.</div>
+                            <div style={{color:"#F2AD00",fontWeight:"bold",fontSize:"20px"}}>Region Multiplier - 60 points</div>
+                            <div style={{ color: 'white' }}>-  This elixir gives you an opportunity to score 1.5x times the points that were previously allotted to the questions. This elixir is valid ONLY for all questions in that particular region. The multiplier starts from the question, at which it is applied.
+</div>
+<div style={{color:"#F2AD00",fontWeight:"bold",fontSize:"20px"}}>Hangman - 20 points</div>
+                            <div style={{ color: 'white' }}>-  Hangman like the classic game gives you the letters of the answer with blanks in between. This way you get a chance to get to the answer, using a few letters of the actual answer!
+</div>
+<div style={{color:"#F2AD00",fontWeight:"bold",fontSize:"20px"}}>Skip Question -50 Points</div>
+                            <div style={{ color: 'white' }}>-  For the first time ever, Hawk gives you the opportunity to skip a question! Remember that this will not count as an answered question, and points will not be added for it.
+</div>
+					<div style={{fontWeight:"bold",textAlign:"center",fontSize:"25px"}}>For any issues, contact one of the following</div>
 						<div className="Contact">
 						
-							<h4>Swadhin Routray 7676598351</h4>
-							<h4>Chakradhar Reddy 9515334754</h4>
-							<h4>Keoul Patel 8291416388</h4>
-							<h4>Dennis Johnson 9048044761</h4>
-							<h4>Sanchit Sahay 8817137466</h4>
+							<div  className="single-contact"><div>Swadhin Routray</div><div>7676598351</div> </div>
+							<div className="single-contact"><div>Chakradhar Reddy</div> <div>9515334754</div></div>
+							<div className="single-contact"><div>Keoul Patel</div><div> 8291416388</div></div>
+							<div className="single-contact"><div>Dennis Johnson</div><div> 9048044761</div></div>
+							<div className="single-contact"><div>Sanchit Sahay</div> <div>8817137466</div></div>
+                            <div className="single-contact"><div>Devesh Mahajan</div> <div>9953512577</div></div>
 						
 						</div>
+					</div>
+
 						</div>
 				</div>
 			</RulesWrapper>
@@ -143,7 +147,7 @@ const RulesWrapper = styled.div`
 		bottom: 2%;
 		left: 25%;
 		right: 25%;
-
+       
 		padding: 3rem;
 		::-webkit-scrollbar {
 			display: none;
@@ -190,10 +194,40 @@ const RulesWrapper = styled.div`
 		text-align:left;
 	}
 	.Contact{
+        *{
+            color:white;
+        }
+
+        margin:0 auto;
+        
 		display:flex;
-		flex-wrap:wrap;
-		font-size:10px;
+        
+        
+		font-size: 20px !important;
+        
+		flex-wrap:nowrap;
+        flex-direction:column;
+        align-items:center;
+        text-align:center;
+
+    
+        
 	}
+    .Contact>.single-contact{
+        margin:1rem auto;
+       font-weight:800;
+        
+            display:flex;
+            flex-direction:column;
+        align-items:center;
+        text-align:center;
+        margin:0.3rem;
+        
+        
+    }
+    .Contact>h4>div{
+        margin:0;
+    }
 	@media (max-width: ${size.laptop}) {
 		.Rules {
 			left: 5%;
